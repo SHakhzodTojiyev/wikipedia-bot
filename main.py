4d7d56f02fcbd8e5a9b715db0ff1efff5ff05d38
Shakhzod Tojiyev <68785236+SHakhzodTojiyev@users.noreply.github.com>
2022-02-17 08:43:46 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import logging
+import wikipedia
+
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = 'Qovuncha'
+wikipedia.set_lang('uz')
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Salom bro 😎\nBu wikipedia bot!\nMendan lyuboy narsani so'rang...")
+
+@dp.message_handler()
+async def sendWiki(message: types.Message):
+    try:
+        respond = wikipedia.summary(message.text)
+        await message.answer(respond)
+    except:
+        await message.answer("Bu mavzuga oid ma'qola topilmadi. 😔") 
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)   
